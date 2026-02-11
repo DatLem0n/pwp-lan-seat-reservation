69e29edf79cbe4d555f0ecccdc92408d4e162e95
Eemeli Kyröläinen <eemeli.ky@gmail.com>
2026-02-11 14:19:58 +0200

    Lisätty jpa dependyt ja user luominen

--- seat-reservation/src/main/java/com/fragment/seat_reservation/models/User.java
+++ seat-reservation/src/main/java/com/fragment/seat_reservation/models/User.java
@@ -1,16 +1,29 @@
 package com.fragment.seat_reservation.models;
-
+import java.time.LocalDate;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "users")
+@Table(name = "Users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private int id;
     private String username;
-    private String password;
-
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phone;
+    private boolean isAdmin;
+    private LocalDate dob;
+    private LocalDate createdAt;
+    private LocalDate lastLogin;
 }
 // user database "schema"
