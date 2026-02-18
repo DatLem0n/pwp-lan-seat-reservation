04b9c6973e428a2a3fd2f87d887aaa8333b17553
huntilla <kalleasmundi@gmail.com>
2026-02-18 16:18:10 +0200

    Created test user registration endpoint, UserDto and UserService

--- seat-reservation/src/main/java/com/fragment/seat_reservation/services/UserService.java
+++ seat-reservation/src/main/java/com/fragment/seat_reservation/services/UserService.java
@@ -1,5 +1,36 @@
 package com.fragment.seat_reservation.services;
 
+import com.fragment.seat_reservation.dto.UserDto;
+import com.fragment.seat_reservation.entities.User;
+import com.fragment.seat_reservation.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void saveUser (UserDto userDto) {
+       User user = new User();
+       user.setUsername(userDto.getUsername());
+       user.setFirstName(userDto.getFirstName());
+       user.setLastName(userDto.getLastName());
+       user.setEmail(userDto.getEmail());
+       user.setPassword(userDto.getPassword());
+       user.setPhone(userDto.getPhoneNumber());
+
+       user.setDob(java.time.LocalDate.now());
+       user.setLastLogin(null);
+       user.setAdmin(true);
+       user.setCreationDate(java.time.LocalDate.now());
+
+       userRepository.save(user);
+    }
+
+
 }
 // business logic for users / auth
