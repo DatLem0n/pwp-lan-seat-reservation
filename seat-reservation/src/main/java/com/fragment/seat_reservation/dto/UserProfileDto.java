164b1d7c52f66a6367ec1d88e692c367cebfea36
huntilla <kalleasmundi@gmail.com>
2026-05-03 19:01:49 +0300

    Add id to user GET request. Change dob to dateOfBirth in UerRegistrationDto

--- seat-reservation/src/main/java/com/fragment/seat_reservation/dto/UserProfileDto.java
+++ seat-reservation/src/main/java/com/fragment/seat_reservation/dto/UserProfileDto.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 public class UserProfileDto {
+    private Long id;
     private String username;
     private String firstName;
     private String lastName;
