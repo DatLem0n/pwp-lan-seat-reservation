d06fa6fcee56e508803769f850e2c5acb64fc43e
huntilla <kalleasmundi@gmail.com>
2026-05-03 19:17:48 +0300

    Modify user deletion logic to remove users reservations before deleting user

--- seat-reservation/src/main/java/com/fragment/seat_reservation/repositories/SeatRepository.java
+++ seat-reservation/src/main/java/com/fragment/seat_reservation/repositories/SeatRepository.java
@@ -1,12 +1,19 @@
 package com.fragment.seat_reservation.repositories;
 
 import com.fragment.seat_reservation.entities.Seat;
+import com.fragment.seat_reservation.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
     List<Seat> findAllByLocationId(Long locationId);
     Seat findTopByLocationIdOrderBySeatNumberDesc (Long id);
+
+    @Modifying
+    @Query("update Seat s set s.reservedFor = null where s.reservedFor = :user")
+    void clearReservationsForUser(@Param("user") User user);
 }
